7dd2124d2d673d56898bf5062bcbf1b286164514
mgkaungkhantzaw <kaungkhantzaw2001326@gmail.com>
2022-12-22 23:15:06 +0630

    finish

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const { ObjectId } = require('mongodb')
 const { connectToDb, getDb } =  require('./db')
 
 // app init & middleware
 const app = express()
+app.use(express.json())
 
 //db
 let db
@@ -20,16 +22,85 @@ connectToDb((err) => {
 //get
 app.get('/', (req, res) => {
     // res.json({ msg: "hello mongodb and express"})
+    let page = req.query.p || 0
+    let BookPerPage = 3
     
     let books = []
     db.collection('books')
         .find()
         .sort({ author: 1 })
+        .skip(page * BookPerPage) //pagination
+        .limit(BookPerPage)
         .forEach(book => books.push(book))
         .then(() => {
-            res.status(200).json(books)
+            res.status(200).json({ data: books})
         })
         .catch(() => {
             res.status(500).json({error: "cannot fetch data from documents"})
         })
+})
+
+
+
+//find id
+app.get('/:id', (req, res) => {
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('books')
+        .findOne({_id: ObjectId(req.params.id)})
+        .then(result => {
+            res.json({ data: result })
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Could not fetch document'})
+        })
+    }else{
+        res.status(500).json({ error: 'invalid id'})
+    }
+})
+
+//post method to create
+app.post('/create', (req, res) => {
+    let book = req.body
+    db.collection('books')
+        .insertOne(book)
+        .then(result => {
+            res.json({data: result, msg: "successfully inserted"})
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Could not insert data'})
+        })
+})
+
+
+//delete
+app.delete('/:id', (req, res) => {
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('books')
+        .deleteOne({_id: ObjectId(req.params.id)})
+        .then(result => {
+            res.json({ data: result, msg: "successfully deleted" })
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Could not fetch document'})
+        })
+    }else{
+        res.status(500).json({ error: 'invalid id'})
+    }
+})
+
+//update
+app.patch('/:id', (req, res) => {
+    let update = req.body
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('books')
+        .updateOne({_id: ObjectId(req.params.id)}, {$set: update })
+        .then(result => {
+            res.json({ data: result, msg: "successfully updated" })
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Could not fetch document'})
+        })
+    }else{
+        res.status(500).json({ error: 'invalid id'})
+    }
 })
